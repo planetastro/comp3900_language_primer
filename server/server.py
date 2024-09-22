5b724c6cff647d0bc80ebc630d062f79f1ab3c41
William Setiawan <willc0d3r@gmail.com>
2024-09-22 19:22:34 +1000

    Added edge cases

--- server/server.py
+++ server/server.py
@@ -1,17 +1,22 @@
-'''
-Edge case:
-    Empty group name / Empty group members?
-Solution:
-    Group class requires a group name to be given?
+"""
+Student Management System: Allows easy organisation of students into groups, tracking group/student names and quantity.
 
 Assumptions:
-    - Group IDs are unique
-    - Member IDs are unique
-    - When a Group is deleted, all its members are also deleted
-    - There is no maximum number of Groups/Members
-    - Group/Member names can be any character
-'''
-from flask import Flask, jsonify, request, abort
+    - Group/Student IDs are unique
+    - When a Group is deleted, all containing students are also deleted
+    - There is no maximum number of Groups/Students
+    - Group/Student Name must be alphanumeric (e.g., Group 1, Mike! or J3nny)
+
+Edge Cases:
+    - Groups cannot be empty (since Groups cannot be edited)
+        - Aborts with 400
+    - Group/Student Name cannot be empty
+        - Aborts with 400
+    - Users cannot GET/DELETE non-existent Group IDs
+        - Aborts with 404
+"""
+
+from flask import Flask, jsonify, request, abort, Response
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -24,7 +29,8 @@ class Student:
         self.student_id = student_id
         self.name = name
 
-    def to_dict(self) -> dict[str]:
+    def inner(self) -> dict[str]:
+        """Returns a object-format of the given Student."""        
         return { "id": self.student_id, "name": self.name }
 
 class Group:
@@ -35,18 +41,20 @@ class Group:
         self.group_name = group_name
         self.members = members
 
-    def to_summary(self) -> dict[str]:
+    def summary(self) -> dict[str]:
+        """Returns a summary of the given Group."""        
         return {
             "id": self.group_id,
             "groupName": self.group_name,
             "members": [student.student_id for student in self.members]
         }
 
-    def to_dict(self) -> dict[str]:
+    def inner(self) -> dict[str]:
+        """Returns an object-format of the given Group."""        
         return {
             "id": self.group_id,
             "groupName": self.group_name,
-            "members": [student.to_dict() for student in self.members]
+            "members": [student.inner() for student in self.members]
         }
 
 class Groups:
@@ -65,43 +73,60 @@ class Groups:
 
     @property
     def next_group_id(self) -> int:
+        """The next auto-incremented Group ID to be used.
+        NOTE: Fetching this property has the side effect of incrementing it."""
         temp = self._auto_group_id
         self._auto_group_id += 1
         return temp
 
     @property
     def next_member_id(self) -> int:
+        """The next auto-incremented Member ID to be used.
+        NOTE: Fetching this property has the side effect of incrementing it."""
         temp = self._auto_member_id
         self._auto_member_id += 1
         return temp
 
     def group_id_exists(self, group_id: int) -> bool:
+        """Returns True if the given Group ID exists."""        
         return group_id in self._groups
 
-    def add_group(self, group_name: str, group_members: list[str]) -> Group:
-        members = [Student(self.next_member_id, name) for name in group_members]
+    def add_group(self, group_name: str, students: list[str]) -> Group:
+        """Stores a new Group with the given name and students and returns the Group."""        
+        members = [Student(self.next_member_id, name) for name in students]
         new_group = Group(self.next_group_id, group_name, members)
         self._groups[new_group.group_id] = new_group
         return new_group
 
     def delete_group(self, group_id: int) -> bool:
-        # Delete group
+        """Returns True if the given Group ID exists and the specified Group was successfully deleted."""
         if self.group_id_exists(group_id):
             self._groups.pop(group_id)
             return True
         return False
 
     def get_group_dict(self, group_id) -> dict:
-        return self[group_id].to_dict()
+        """Returns the details of a Group."""
+        return self[group_id].inner()
 
     def get_group_summaries(self) -> list[dict]:
-        return [group.to_dict() for group in self._groups.values()]
+        """Returns all the Summaries of the currently-existing Groups."""
+        return [group.summary() for group in self._groups.values()]
 
     def get_members_dict(self) -> list[dict]:
-        return [student.to_dict() for group in self._groups.values() for student in group.members]
+        """Returns the details of all Students."""
+        return [student.inner() for group in self._groups.values() for student in group.members]
 
 def is_invalid_name(name: str) -> bool:
-    return not name.isalpha()
+    """Returns True if the given name is empty or contains non-alphanumeric characters."""
+    name = name.strip()
+    return not (len(name) > 0 and all(char.isalnum() or char.isspace() for char in name))
+
+def create_json_response(data: any) -> Response:
+    """Returns a Response object with the given data parsed into JSON format and logged."""
+    response = jsonify(data)
+    app.logger.debug(response.json)
+    return response
 
 all_groups = Groups()
 
@@ -111,9 +136,7 @@ def get_groups():
     Route to get all groups
     return: Array of group objects
     """
-    response = jsonify(all_groups.get_group_summaries())
-    app.logger.debug(response.json)
-    return response
+    return create_json_response(all_groups.get_group_summaries())
 
 @app.route('/api/students', methods=['GET'])
 def get_students():
@@ -121,9 +144,7 @@ def get_students():
     Route to get all students
     return: Array of student objects
     """
-    response = jsonify(all_groups.get_members_dict())
-    app.logger.debug(response.json)
-    return response
+    return create_json_response(all_groups.get_members_dict())
 
 @app.route('/api/groups', methods=['POST'])
 def create_group():
@@ -139,15 +160,18 @@ def create_group():
     group_name = group_data.get("groupName")
     group_members = group_data.get("members")
 
+    # Edge case 1: Groups cannot be empty
+    if len(group_members) == 0:
+        abort()
+
+    # Check if any name is invalid
+    for name in [group_name, *group_members]:
+        if is_invalid_name(name):
+            abort(400, f"The following name is invalid: {name}")
+
     # Add a new group
-    if any(is_invalid_name(name) for name in [group_name, *group_members]):
-        abort(400, 'A name given is not fully alphabetical')
-
     new_group = all_groups.add_group(group_name, group_members)
-
-    response = jsonify(new_group.to_dict())
-    app.logger.debug(response.json)
-    return response, 201
+    return create_json_response(new_group.inner()), 201
 
 @app.route('/api/groups/<int:group_id>', methods=['DELETE'])
 def delete_group(group_id: int):
@@ -172,9 +196,7 @@ def get_group(group_id: int):
     if not all_groups.group_id_exists(group_id):
         abort(404, 'Group not found')
 
-    response = jsonify(all_groups.get_group_dict(group_id))
-    app.logger.debug(response.json)
-    return response
+    return create_json_response(all_groups.get_group_dict(group_id))
 
 if __name__ == '__main__':
     app.run(port=3902, debug=True)
